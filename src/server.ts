8c0915e31325d4f515bcee138ffc5670ba0d3357
Adamo Buosi <adamo.buosi@gmail.com>
2020-10-19 10:56:53 -0300

    ajuste no campo booleano

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,20 @@
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import 'express-async-errors';
+
+import './database/connection';
+import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-// Query Param -> http://localhost:3333/users?search=diego
-// Route Param -> http://localhost:3333/users/1
-// Body -> http://localhost:3333/users
-
-app.get('/users', (request, response) => {
-    console.log(request.query);
-    console.log(request.params);
-    console.log(request.body);
-    return response.json({message: "Hello World!"});
-})
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
+
